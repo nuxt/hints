e0c8070a6ada3b9f0db740f06a11bd90fe274f7b
Julien Huang <julien.h.dev@gmail.com>
2025-03-20 23:42:42 +0100

    feat: devtools

---
+++ client/pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useDevtoolsClient } from '@nuxt/devtools-kit/iframe-client'
+
+const client = useDevtoolsClient()
+</script>
+
+<template>
+  <div class="relative p-10 n-bg-base flex flex-col h-screen">
+    Hints devtools
+  </div>
+</template>
--- src/runtime/plugins/performance/plugin.client.ts
+++ src/runtime/plugins/performance/plugin.client.ts
@@ -1,4 +1,4 @@
-type PerfEntry = PerformanceEntry & { element: any };
+type PerfEntry = PerformanceEntry & { element: HTMLElement };
 type ElementNode = ChildNode & { attributes: { href: { value: string } } };
 import { defineNuxtPlugin } from "#imports";
 
