c5122518a1ac73f1c3cce907b3032042da7df0d4
Julien Huang <julien.h.dev@gmail.com>
2025-04-27 14:29:06 +0200

    fix: use type augmentation and remove nuxt provide

--- client/app/composables/vue-tracer.ts
+++ client/app/composables/vue-tracer.ts
@@ -2,7 +2,7 @@ import { useDevtoolsClient } from '@nuxt/devtools-kit/iframe-client'
 import type { VNode } from 'vue'
 import type { findTraceFromElement as _findTraceFromElement } from 'vite-plugin-vue-tracer/client/record'
 
-export function findClosestTraceInfo(el: Element & { __vnode: VNode }) {
+export function findClosestTraceInfo(el: Element) {
   const client = useDevtoolsClient().value
   if (!client || !client.host.inspector) {
     throw new Error('`findClosestWithTraceInfo` must be used when the devtools client is connected')
--- src/runtime/plugins/vue-tracer-state.client.ts
+++ src/runtime/plugins/vue-tracer-state.client.ts
@@ -1,10 +1,6 @@
 import * as tracerOverlay from 'vite-plugin-vue-tracer/client/overlay'
 import { defineNuxtPlugin } from '#imports'
 
-export default defineNuxtPlugin(() => {
-  return {
-    provide: {
-      tracerOverlay,
-    },
-  }
+export default defineNuxtPlugin((nuxtApp) => {
+    nuxtApp.__tracerOverlay = tracerOverlay
 })
