8beaf1fddbd70cf7dfb3b10231f9eba93ce083ec
Julien Huang <julien.h.dev@gmail.com>
2025-06-27 10:17:29 +0200

    Update src/runtime/plugins/third-party-scripts/plugin.client.ts

--- src/runtime/plugins/third-party-scripts/plugin.client.ts
+++ src/runtime/plugins/third-party-scripts/plugin.client.ts
@@ -4,7 +4,7 @@ export default defineNuxtPlugin((nuxtApp) => {
   const scripts = document.scripts
   for (const script of scripts) {
     // If the script has another origin, we warn about using @nuxt/scripts
-    if (script.src && script.src.startsWith('http') && !script.src.includes(nuxtApp.$config.public.baseURL) && !script.attributes.getNamedItem('async')) {
+    if (script.src && script.src.startsWith('http') && !script.src.includes(nuxtApp.$config.public.baseURL) && !script.attributes.getNamedItem('defer')) {
       console.warn(`[@nuxt/scripts] Script from another origin detected: ${script.src}. Consider using @nuxt/scripts.`)
     }
   }
