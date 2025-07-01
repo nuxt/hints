c9034641ef96d5e878e4d1d1fa1a06f9b9d694e9
Julien Huang <julien.h.dev@gmail.com>
2025-07-01 21:58:58 +0200

    feat: warn crossorigin

--- src/runtime/plugins/third-party-scripts/plugin.client.ts
+++ src/runtime/plugins/third-party-scripts/plugin.client.ts
@@ -19,10 +19,13 @@ export default defineNuxtPlugin((nuxtApp) => {
         hasThirdPartyScript = true
         const navigationEntry = performance.getEntriesByType('navigation')[0] as PerformanceNavigationTiming
         script.__hints_TPC_start_time = (navigationEntry ? performance.timeOrigin + navigationEntry.domContentLoadedEventStart : window.__hints_TPC_start_time) || window.__hints_TPC_start_time
+        if(!script.crossOrigin) {
+          console.warn(`[@nuxt/hints]: Third-party script "${script.src}" is missing crossorigin attribute. Consider adding crossorigin="anonymous" for better security and error reporting.`)
+        }
       }
     }
     if(hasThirdPartyScript) {
-      console.info('[@nuxt/hints]: 📊 Third-party scripts detected on page load: consider using @nuxt/scripts')
+      console.info('ℹ️ [@nuxt/hints]: Third-party scripts detected on page load: consider using @nuxt/scripts')
     }
   })
 })
