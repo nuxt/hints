ea7a2a51c7ff3083a5cc4319055cfc0d67593c45
Julien Huang <julien.h.dev@gmail.com>
2025-07-31 20:29:00 +0200

    fix: disable module in build

--- src/module.ts
+++ src/module.ts
@@ -4,7 +4,6 @@ import { InjectHydrationPlugin } from './plugins/hydration'
 
 // Module options TypeScript interface definition
 export interface ModuleOptions {
-  enabled: boolean
   devtools: boolean
 }
 
@@ -14,38 +13,40 @@ export default defineNuxtModule<ModuleOptions>({
     configKey: 'hints',
   },
   defaults: {
-    enabled: false,
+    devtools: true,
   },
   setup(options, nuxt) {
+    if (!nuxt.options.dev) {
+      return
+    }
+
     const resolver = createResolver(import.meta.url)
 
-    if (options.enabled || process.env.NODE_ENV === 'development') {
-      // performances
-      addPlugin(resolver.resolve('./runtime/plugins/performance/plugin.client'))
+    // performances
+    addPlugin(resolver.resolve('./runtime/plugins/performance/plugin.client'))
 
-      // hydration
-      addPlugin(resolver.resolve('./runtime/plugins/hydration/plugin.client'))
-      addBuildPlugin(InjectHydrationPlugin)
-      addComponent({
-        name: 'NuxtIsland',
-        filePath: resolver.resolve('./runtime/components/nuxt-island.ts'),
-        priority: 1000,
+    // hydration
+    addPlugin(resolver.resolve('./runtime/plugins/hydration/plugin.client'))
+    addBuildPlugin(InjectHydrationPlugin)
+    addComponent({
+      name: 'NuxtIsland',
+      filePath: resolver.resolve('./runtime/components/nuxt-island.ts'),
+      priority: 1000,
+    })
+
+    // third-party scripts
+    addPlugin(resolver.resolve('./runtime/plugins/third-party-scripts/plugin.client'))
+    addServerPlugin(resolver.resolve('./runtime/plugins/third-party-scripts/nitro.plugin'))
+
+    nuxt.hook('prepare:types', ({ references }) => {
+      references.push({
+        types: resolver.resolve('./runtime/types.d.ts'),
       })
+    })
 
-      // third-party scripts
-      addPlugin(resolver.resolve('./runtime/plugins/third-party-scripts/plugin.client'))
-      addServerPlugin(resolver.resolve('./runtime/plugins/third-party-scripts/nitro.plugin'))
-
-      nuxt.hook('prepare:types', ({ references }) => {
-        references.push({
-          types: resolver.resolve('./runtime/types.d.ts'),
-        })
-      })
-
-      if (options.devtools) {
-        setupDevToolsUI(nuxt, resolver)
-        addPlugin(resolver.resolve('./runtime/plugins/vue-tracer-state.client'))
-      }
+    if (options.devtools) {
+      setupDevToolsUI(nuxt, resolver)
+      addPlugin(resolver.resolve('./runtime/plugins/vue-tracer-state.client'))
     }
   },
 })
