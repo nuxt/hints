d52b62596f150184ffbe4934033e72cd367ee262
Julien Huang <julien.h.dev@gmail.com>
2025-11-28 23:10:01 +0100

    refactor: use variables

--- client/app/plugins/hydration.ts
+++ client/app/plugins/hydration.ts
@@ -1,5 +1,6 @@
 import type { HydrationMismatchPayload, HydrationMismatchResponse, LocalHydrationMismatch } from '../../../src/runtime/hydration/types'
 import { defineNuxtPlugin, useHostNuxt, ref } from '#imports'
+import { HYDRATION_ROUTE, HYDRATION_SSE_ROUTE } from '../../../src/runtime/hydration/utils'
 
 export default defineNuxtPlugin(() => {
   const host = useHostNuxt()
@@ -8,11 +9,11 @@ export default defineNuxtPlugin(() => {
 
   hydrationMismatches.value = [...host.__hints.hydration]
 
-  $fetch<HydrationMismatchResponse>(new URL('/__nuxt_hydration', window.location.origin).href).then((data: { mismatches: HydrationMismatchPayload[] }) => {
+  $fetch<HydrationMismatchResponse>(new URL(HYDRATION_ROUTE, window.location.origin).href).then((data: { mismatches: HydrationMismatchPayload[] }) => {
     hydrationMismatches.value = [...hydrationMismatches.value, ...data.mismatches.filter(m => !hydrationMismatches.value.some(existing => existing.id === m.id))]
   })
 
-  const eventSource = new EventSource(new URL('/__nuxt_hydration/sse', window.location.origin).href)
+  const eventSource = new EventSource(new URL(HYDRATION_SSE_ROUTE, window.location.origin).href)
   eventSource.addEventListener('hints:hydration:mismatch', (event) => {
     const mismatch: HydrationMismatchPayload = JSON.parse(event.data)
     if (!hydrationMismatches.value.some(existing => existing.id === mismatch.id)) {
