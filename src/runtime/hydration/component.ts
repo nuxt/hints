1823ead8be6bbbba66f02b85bc5916e54fd7367c
Julien Huang <julien.h.dev@gmail.com>
2025-12-07 19:42:11 +0100

    fix :imports

--- src/runtime/hydration/component.ts
+++ src/runtime/hydration/component.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { defineNuxtComponent as _defineNuxtComponent, defineComponent as _defineComponent, type DefineComponent } from '#imports'
+import { defineNuxtComponent as _defineNuxtComponent, defineComponent as _defineComponent } from '#imports'
 import { useHydrationCheck } from './composables'
+import type { DefineComponent } from 'vue'
 
 export const defineNuxtComponent: typeof _defineComponent
   = function defineNuxtComponent(...args: any[]): any {
