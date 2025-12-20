54ac7514f43c7c695f3416a54ab687e63acb39b6
Julien Huang <julien.h.dev@gmail.com>
2025-12-20 23:36:53 +0100

    fix: improve line changes in hydration diff

--- playground/pages/hydration.vue
+++ playground/pages/hydration.vue
@@ -4,6 +4,15 @@ const isServer = import.meta.server ? 1 : 0
 
 <template>
   <div>
-    <p>isServer: {{ isServer }}</p>
+    <p :data-test="isServer ? 'server' : 'client'" class="fzejkebkzjefbjkzebfkjzebfkjzebfzebfkjzebkfjbzekjjjjjjjjjjjjjjjjjjjjkfzebjkfbzkjebfjkjk">
+      isServer: {{ isServer }} <span v-if="!isServer">test</span>
+    </p>
+
+    <div
+      :class="isServer ? '1561561' : 'text-green-500'"
+      :data-test="isServer ? 'server' : 'client'"
+    >
+      yolooo
+    </div>
   </div>
 </template>
