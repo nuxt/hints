59516d41e9ea0de22089b17503701044417a5600
Julien Huang <julien.h.dev@gmail.com>
2026-03-28 00:21:07 +0100

    chore(deps): update all non-major dependencies (#308)

--- test/fixtures/basic/nuxt.config.ts
+++ test/fixtures/basic/nuxt.config.ts
@@ -4,7 +4,14 @@ export default defineNuxtConfig({
   modules: [
     MyModule,
   ],
+  build: {
+    transpile: ['@nuxt/icon'],
+  },
   experimental: {
     appManifest: false,
+  },
+  // @ts-expect-error unocss types are not available in this fixture
+  unocss: {
+    autoImport: false,
   },
 })
