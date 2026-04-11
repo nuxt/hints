7f48fc08d85c772040079e79f4b2ef4d12d0b589
fabkho <fab.kirchhoff98@gmail.com>
2026-04-11 16:39:07 +0200

    fix: respect app.baseURL in devtools iframe src

--- src/devtools.ts
+++ src/devtools.ts
@@ -3,6 +3,7 @@ import { existsSync } from 'node:fs'
 import type { Nuxt } from '@nuxt/schema'
 import { addDevServerHandler, type Resolver } from '@nuxt/kit'
 import { proxyRequest, eventHandler } from 'h3'
+import { joinURL } from 'ufo'
 
 const DEVTOOLS_UI_ROUTE = '/__nuxt-hints'
 const DEVTOOLS_UI_LOCAL_PORT = 3300
@@ -38,7 +39,7 @@ export function setupDevToolsUI(nuxt: Nuxt, resolver: Resolver) {
     category: 'analyze',
     view: {
       type: 'iframe',
-      src: DEVTOOLS_UI_ROUTE,
+      src: joinURL(nuxt.options.app?.baseURL || '/', DEVTOOLS_UI_ROUTE),
     },
   }, nuxt)
 }
