f0c9b16fc807436dd7a0bd6797d644f54dd0bb9e
Nestor Vera <nestorvera@me.com>
2026-07-09 22:43:07 +0200

    chore: configure vite `optimizeDeps` and bump deps in `.nuxtrc` (#365)

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -3,11 +3,21 @@ export default defineNuxtConfig({
   modules: ['@nuxt/eslint', '@nuxt/hints'],
   devtools: { enabled: true },
   compatibilityDate: '2025-02-26',
+  vite: {
+    optimizeDeps: {
+      include: [
+        '@vue/devtools-core',
+        '@vue/devtools-kit',
+        'vite-plugin-vue-tracer/client/overlay',
+        'vite-plugin-vue-tracer/client/record',
+        'web-vitals/attribution',
+      ],
+    },
+  },
   eslint: {
     config: {
       stylistic: true,
     },
   },
-  hints: { devtools: true,
-  },
+  hints: { devtools: true },
 })
